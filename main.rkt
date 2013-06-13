dc64266c87b0d10b53e0e0581cf604c29baa6352
Richard Harrington <harrington.richard@gmail.com>
2013-06-13 12:16:02 -0400

    added tests and modulified it

--- main.rkt
+++ main.rkt
@@ -1,21 +1,34 @@
 #lang racket
 (require racket/match)
 
-(define evaluate
-  (lambda (expr)
-    (match expr
-      [ (list 'num n) n ]
-      [ (list 'plus left right) (+ (evaluate left) (evaluate right)) ]
-      [ (list 'minus left right) (- (evaluate left) (evaluate right)) ])))
+(module interpreter racket
+  (provide evaluate)
+  (define evaluate
+    (lambda (expr)
+      (match expr
+        [ (list 'num n) n ]
+        [ (list 'plus left right) (+ (evaluate left) (evaluate right)) ]
+        [ (list 'minus left right) (- (evaluate left) (evaluate right)) ]))))
 
-(evaluate (list 'num 7))
-(evaluate (list 'plus (list 'num 5) (list 'num 3)))
-(evaluate (list 'plus (list 'num 5)
-                      (list 'plus (list 'num 2)
-                                  (list 'num 10))))
-
-(evaluate (list 'plus (list 'num 5)
-                      (list 'minus (list 'num 2)
-                                   (list 'num 1))))
-
+(module test racket
+  (require rackunit)
+  (test-equal? "evaluate to 7"
+               (evaluate (list 'num 7)) 
+               7)
+  
+  (test-equal? "simple addition"
+               (evaluate (list 'plus (list 'num 5) (list 'num 3)))
+               8)
+  
+  (test-equal? "nested addition" 
+               (evaluate (list 'plus (list 'num 5)
+                               (list 'plus (list 'num 2)
+                                     (list 'num 10))))
+               17)
+  
+  (test-equal? "addition and subtraction"
+               (evaluate (list 'plus (list 'num 5)
+                               (list 'minus (list 'num 2)
+                                     (list 'num 1))))
+               6))
 
