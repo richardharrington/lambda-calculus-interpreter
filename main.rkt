977366f43bfb2fe6b4a921940bacb4babe422219
Richard Harrington <harrington.richard@gmail.com>
2013-06-14 11:44:45 -0400

    removed clunky 'list' syntax from tests; added higher-order function test

--- main.rkt
+++ main.rkt
@@ -2,68 +2,87 @@
 
 (module interpreter racket
   (require racket/match)
-  (provide evaluate)
+  (provide eval-with-env evaluate)
   
   (define evaluate
+    (lambda (expr)
+      (eval-with-env expr '())))
+  
+  (define eval-with-env
     (lambda (expr env)
       (match expr
         [ (list 'var name) (lookup name env)]
         [ (list 'num n) n ]
-        [ (list 'plus left right) (+ (evaluate left env) (evaluate right env)) ]
-        [ (list 'minus left right) (- (evaluate left env) (evaluate right env)) ]
+        [ (list 'plus left right) (+ (eval-with-env left env) (eval-with-env right env)) ]
+        [ (list 'minus left right) (- (eval-with-env left env) (eval-with-env right env)) ]
         [ (list 'func _ _) (list 'closure expr env) ]
-        [ (list 'call f val) (match (evaluate f env)
+        [ (list 'call c val) (match (eval-with-env c env)
                              [ (list 'closure (list 'func var-name body) cenv) 
-                               (evaluate body (cons (list var-name (evaluate val env)) cenv)) ]
-                             [ _ (raise-arguments-error 'Eval: "Cannot Evaluate Call" "expr: " f) ])])))
+                               (eval-with-env body (cons (list var-name (eval-with-env val env)) cenv)) ]
+                             [ _ (raise-arguments-error 'Eval: "Cannot eval-with-env Call" "expr: " c) ])])))
   
   (define lookup
     (lambda (var env)
       (cond
         [(null? env) (error "unbound variable")]
         [(eq? (first (car env)) var) (second (car env))]
-        [else (lookup var (cdr env))])))
-          
-  )
+        [else (lookup var (cdr env))]))))
 
 
 (module test racket
   (require rackunit)
   (require (submod ".." interpreter))
   
-  (test-equal? "functions"
-               (evaluate (list 'call (list 'func "x" (list 'plus (list 'var "x") (list 'num 1)))
-                                     (list 'num 2)) '())
-               3)
-  
   (test-equal? "evaluate to 7"
-               (evaluate (list 'num 7) '()) 
+               (evaluate '(num 7)) 
                7)
   
   (test-equal? "simple addition"
-               (evaluate (list 'plus (list 'num 5) (list 'num 3)) '())
+               (evaluate '(plus (num 5) (num 3)))
                8)
   
   (test-equal? "nested addition"
-               (evaluate (list 'plus (list 'num 5)
-                               (list 'plus (list 'num 2)
-                                     (list 'num 10))) '())
+               (evaluate '(plus (num 5)
+                                (plus (num 2)
+                                      (num 10))))
                17)
   
   (test-equal? "addition and subtraction"
-               (evaluate (list 'plus (list 'num 5)
-                               (list 'minus (list 'num 2)
-                                     (list 'num 1))) '())
+               (evaluate '(plus (num 5)
+                                (minus (num 2)
+                                       (num 1))))
                6)
 
-  (test-equal? "evaluate a variable"
-               (evaluate (list 'var "a") '(("a" 5)))
+  (test-equal? "eval-with-env a variable"
+               (eval-with-env '(var "a") '(("a" 5)))
                5)
   
   (test-equal? "addition and subtraction with variable"
-               (evaluate (list 'plus (list 'var "a")
-                               (list 'minus (list 'num 2)
-                                     (list 'num 1))) '(("a" 5)))
-               6))
+               (eval-with-env '(plus (var "a")
+                                (minus (num 2)
+                                       (num 1))) '(("a" 5)))
+               6)
+
+  (test-equal? "functions"
+               (evaluate '(call (func "x" 
+                                      (plus (var "x") 
+                                            (num 1)))
+                                (num 2)))
+               3)
+  
+  (test-equal? "passing functions to functions"
+               (evaluate '(call (func "f"
+                                      (plus (num 2)
+                                            (call (var "f")
+                                                  (num 8))))
+                                (func "x"
+                                      (plus (num 5)
+                                            (var "x")))))
+               15)
+  
+  )
+               
+
+
  
 
